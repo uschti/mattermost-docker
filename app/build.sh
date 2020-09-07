00b9eba1a5ed5fd8620d083dc8867458b10870c0
uschti <uschti@gmail.com>
2020-09-07 18:09:00 +0200

    Add files via upload

---
+++ app/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+version=5.26.1
+edition=team
+tagName=uschti/mattermost-prod-app
+push=$1
+
+docker image build --build-arg MM_VERSION=${version} --build-arg edition=${edition} -t ${tagName} -t ${tagName}:${version} .
+
+if [[ -z "$push" || $push == true ]]
+then
+	echo "Pushing to Docker HUB"
+    docker push ${tagName} && \
+    docker push ${tagName}:${version}
+fi
